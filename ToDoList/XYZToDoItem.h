55b2540a0b5add6ef51ef860b9229848a0968e11
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-01-10 22:57:18 -0500

    Added data model

---
+++ ToDoList/XYZToDoItem.h
@@ -0,0 +1,17 @@
+//
+//  XYZToDoItem.h
+//  ToDoList
+//
+//  Created by Anojh Gnanachandran on 1/10/2014.
+//  Copyright (c) 2014 Anojh Gnanachandran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZToDoItem : NSObject
+
+@property NSString *itemName;
+@property BOOL completed;
+@property NSDate *creationDate;
+
+@end
---
+++ ToDoList/XYZToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  XYZToDoItem.m
+//  ToDoList
+//
+//  Created by Anojh Gnanachandran on 1/10/2014.
+//  Copyright (c) 2014 Anojh Gnanachandran. All rights reserved.
+//
+
+#import "XYZToDoItem.h"
+
+@implementation XYZToDoItem
+
+@end
